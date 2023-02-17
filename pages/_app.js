8bf9a5206eb91b7378bdf4f5183eb566959d7e17
clearyed <eddiejcleary@gmail.com>
2023-02-16 19:44:43 -0700

    Context now gets data from useUserData hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
-import "../styles/globals.css";
+import { useUserData } from "@/lib/hooks";
+import { UserContext } from "@/lib/context";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "react-hot-toast";
-import { UserContext } from "@/lib/context";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  const userData = useUserData();
+
   return (
-    <UserContext.Provider value={{ user: {}, username: "jeff" }}>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
