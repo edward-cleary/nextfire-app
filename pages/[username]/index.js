63539efc846da81f91ab875155b7a4cfa2e9a0e2
clearyed <eddiejcleary@gmail.com>
2023-02-18 13:29:48 -0700

    Added SSR to user page

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,12 +1,46 @@
-import { useContext } from "react";
-import { UserContext } from "@/lib/context";
+import UserProfile from "../../components/UserProfile";
+import PostFeed from "../../components/PostFeed";
+import { getUserWithUsername, postToJSON } from "@/lib/firebase";
+import {
+  where,
+  orderBy,
+  limit,
+  collection,
+  getDocs,
+  query as fireQuery,
+} from "firebase/firestore";
 
-export default function Page({}) {
-  const { user, username } = useContext(UserContext);
+export async function getServerSideProps({ query }) {
+  const { username } = query;
 
+  const userDoc = await getUserWithUsername(username);
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = fireQuery(
+      collection(userDoc.ref, "posts"),
+      where("published", "==", true),
+      orderBy("createdAt", "desc"),
+      limit(5)
+    );
+
+    posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  };
+}
+
+export default function UserProfilePage({ user, posts }) {
   return (
     <main>
-      <h1>{`You are ${username}`}</h1>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
     </main>
   );
 }
