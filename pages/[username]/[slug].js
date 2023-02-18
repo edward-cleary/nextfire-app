6f97afae61f4b59d18d74d98ceb2444d9170fde7
clearyed <eddiejcleary@gmail.com>
2023-02-18 16:46:15 -0700

    Created static paths and props for post page

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,3 +1,52 @@
+import { getUserWithUsername } from "@/lib/firebase";
+import { firestore } from "@/lib/firebase";
+import { postToJSON } from "@/lib/firebase";
+import {
+  collectionGroup,
+  getDoc,
+  getDocs,
+  query,
+  doc,
+} from "firebase/firestore";
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let post;
+  let path;
+
+  if (userDoc) {
+    const postRef = doc(userDoc.ref, "posts", slug);
+    post = postToJSON(await getDoc(postRef));
+
+    path = postRef.path;
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 5000,
+  };
+}
+
+export async function getStaticPaths() {
+  // Improve by using Admin SDK to select empty docs
+  const postsQuery = query(collectionGroup(firestore, "posts"));
+  const querySnapshot = await getDocs(postsQuery);
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+}
+
 export default function PostPage({}) {
   return (
     <main>
