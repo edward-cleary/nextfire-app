ca0baa54ce38b069a7b29bb154679575cfe470a2
clearyed <eddiejcleary@gmail.com>
2023-02-17 19:01:39 -0700

    Added username from context

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { UserContext } from "@/lib/context";
+
 export default function Page({}) {
+  const { user, username } = useContext(UserContext);
+
   return (
     <main>
-      <h1>You are here</h1>
+      <h1>{`You are ${username}`}</h1>
     </main>
   );
 }
